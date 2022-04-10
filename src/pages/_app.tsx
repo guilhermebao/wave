0378f5b3afed49dffad918b90776f83d0cd1875b
guilhermecbao <guilhermecbao@outlook.com>
2022-04-10 20:39:30 -0300

    init project basic architecture

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import 'tailwindcss/tailwind.css'
+import { AppProps } from 'next/app'
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
