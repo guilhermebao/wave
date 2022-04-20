1d7db11f0255e6149940f6621a8458f1345e938d
guilhermecbao <guilhermecbao@outlook.com>
2022-04-19 23:02:56 -0300

    init server

---
+++ packages/server/src/server.ts
@@ -0,0 +1,16 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const axios = require('axios')
+
+app.use(cors())
+
+app.get('/', async(req, res) => {
+
+    // response é a resposta do axios mas eu tiro a data de dentro do response
+   const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+   
+   return res.json(data) 
+})
+
+app.listen('4567')
